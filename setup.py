a887d2aa1d48e79a005398e7109df2f0109242e1
Rohit Khadka <rohitkhadka153@gmail.com>
2024-11-03 21:50:50 +0545

    FOLDER STRUCTURE ADDED

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+ 
+
+setup(
+    name = "Gen AI Project",
+    version = '0.0.0',
+    author = 'Rohit Khadka',
+    author_email = 'rohitkhadka153@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
